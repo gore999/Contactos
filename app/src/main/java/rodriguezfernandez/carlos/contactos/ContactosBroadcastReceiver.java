ff88a7c11dac27114805bde3050b8f3ece233241
Carlos <carloscoucho@gmail.com>
2019-05-30 00:42:56 +0200

    Commit: Correcciones menores en comentarios y archivos strings.xml.

--- app/src/main/java/rodriguezfernandez/carlos/contactos/ContactosBroadcastReceiver.java
+++ app/src/main/java/rodriguezfernandez/carlos/contactos/ContactosBroadcastReceiver.java
@@ -8,7 +8,7 @@ public class ContactosBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // Arranca la aplicacion de contactos cuando nota que se conecta unos cascos.
+        // Arranca la aplicacion de contactos cuando nota que se conecta a la corriente.
         Intent in=new Intent(context,MainActivity.class);
         context.startActivity(in);
     }
